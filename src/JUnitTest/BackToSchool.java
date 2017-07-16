2f06f39fd64745797333bc3d02fda705ee5f83da
Hidlebaugh-Scott <compscott@gmail.com>
2017-07-16 15:13:29 -0500

    Add files via upload

---
+++ src/JUnitTest/BackToSchool.java
@@ -0,0 +1,24 @@
+package JUnitTest;
+
+import java.util.Arrays;
+
+public class BackToSchool {
+
+	public String[] prepareTheCart() {
+
+		String[] cart = {"TextBooks", "Laptop", "Pens"};
+
+		System.out.println("Cart currently contains: " + Arrays.toString(cart));
+
+		return cart;
+	}
+
+	public String[] addPaperAndPencils() {
+
+		String[] cart = {"TextBooks", "Laptop", "Pens", "Paper", "Pencils"};
+
+		System.out.println("Cart now contains: " + Arrays.toString(cart));
+
+		return cart;
+	}
+}
